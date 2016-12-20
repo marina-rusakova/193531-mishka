a5018ca982f89dadef37ca75285931216e6c10a1
Marina Rusakova <marinarusakova111@gmail.com>
2016-12-20 02:07:27 -0800

    Подготовила адаптивную графику и состояния

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove("main-nav--nojs");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
   }
+});
+
+
+var popup = document.querySelector(".modal-overlay");
+var links = document.querySelectorAll(".modal-open-btn");
+var close = popup.querySelector(".modal-close-btn");
+
+var i = 0;
+
+for (i = 0; i < links.length; ++i) {
+  links[i].addEventListener("click", function(event) {
+      event.preventDefault();
+      popup.classList.add("modal-content-show");
+  });
+}
+
+close.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-content-show");
 });
