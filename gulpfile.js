211dfe794d1270b2af366b750a07e4906fe8b83a
Marina Rusakova <marinarusakova111@gmail.com>
2016-12-23 01:21:13 -0800

    Автоматизация сборки личного проекта 3

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,40 @@ var server = require("browser-sync").create();
 var run = require("run-sequence");
 
 
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("less/**/*.less", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+});
+
+
+var del = require("del");
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+
 gulp.task("style", function() {
   gulp.src("less/style.less")
     .pipe(plumber())
@@ -35,6 +69,7 @@ gulp.task("style", function() {
     .pipe(gulp.dest("build/css"));
 });
 
+
 gulp.task("images", function() {
   return gulp.src("build/img/**/*.{png,jpg,gif}")
     .pipe(imagemin([
@@ -44,37 +79,6 @@ gulp.task("images", function() {
     .pipe(gulp.dest("build/img"));
 });
 
-gulp.task("copy", function() {
-  return gulp.src([
-    "fonts/**/*.{woff,woff2}",
-    "img/**",
-    "js/**",
-    "*.html"
-  ], {
-    base: "."
-  })
-  .pipe(gulp.dest("build"));
-});
-
-var del = require("del");
-
-gulp.task("clean", function() {
-  return del("build");
-});
-
-
-gulp.task("serve", ["style"], function() {
-  server.init({
-    server: ".",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-
-  gulp.watch("less/**/*.less", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
-});
 
 gulp.task("build", function(fn) {
   run(
